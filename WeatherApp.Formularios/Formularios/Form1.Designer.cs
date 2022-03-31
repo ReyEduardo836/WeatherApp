fac006e1700daee8b9ebf596b7f62f1ec62dfd47
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2022-03-30 23:49:41 -0600

    Proyecto finalizado

--- WeatherApp.Formularios/Formularios/Form1.Designer.cs
+++ WeatherApp.Formularios/Formularios/Form1.Designer.cs
@@ -31,11 +31,16 @@ namespace WeatherApp.Formularios.Formularios
         {
             this.cmbPaises = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.gbInfo = new System.Windows.Forms.GroupBox();
+            this.lblHumedad = new System.Windows.Forms.Label();
+            this.lblTemperatura = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gbInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // cmbPaises
@@ -50,63 +55,107 @@ namespace WeatherApp.Formularios.Formularios
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 39);
+            this.label1.Location = new System.Drawing.Point(45, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(47, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 139);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label1.Text = "Tiempo";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 106);
+            this.label3.Location = new System.Drawing.Point(32, 111);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(60, 15);
             this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.label3.Text = "Humedad";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(19, 74);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(73, 15);
             this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.label4.Text = "Temperatura";
             // 
             // gbInfo
             // 
+            this.gbInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbInfo.Controls.Add(this.lblHumedad);
+            this.gbInfo.Controls.Add(this.lblTemperatura);
+            this.gbInfo.Controls.Add(this.lblTiempo);
             this.gbInfo.Controls.Add(this.label1);
-            this.gbInfo.Controls.Add(this.label2);
             this.gbInfo.Controls.Add(this.label3);
             this.gbInfo.Controls.Add(this.label4);
             this.gbInfo.Location = new System.Drawing.Point(235, 33);
             this.gbInfo.Name = "gbInfo";
-            this.gbInfo.Size = new System.Drawing.Size(305, 230);
+            this.gbInfo.Size = new System.Drawing.Size(452, 250);
             this.gbInfo.TabIndex = 5;
             this.gbInfo.TabStop = false;
             this.gbInfo.Text = "Informacion";
+            // 
+            // lblHumedad
+            // 
+            this.lblHumedad.AutoSize = true;
+            this.lblHumedad.Location = new System.Drawing.Point(123, 111);
+            this.lblHumedad.Name = "lblHumedad";
+            this.lblHumedad.Size = new System.Drawing.Size(42, 15);
+            this.lblHumedad.TabIndex = 7;
+            this.lblHumedad.Text = "-------";
+            // 
+            // lblTemperatura
+            // 
+            this.lblTemperatura.AutoSize = true;
+            this.lblTemperatura.Location = new System.Drawing.Point(123, 77);
+            this.lblTemperatura.Name = "lblTemperatura";
+            this.lblTemperatura.Size = new System.Drawing.Size(42, 15);
+            this.lblTemperatura.TabIndex = 6;
+            this.lblTemperatura.Text = "-------";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(123, 38);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(42, 15);
+            this.lblTiempo.TabIndex = 5;
+            this.lblTiempo.Text = "-------";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(50, 160);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 6;
+            this.btnConfirm.Text = "Confirmar";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(50, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 379);
+            this.ClientSize = new System.Drawing.Size(699, 295);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.gbInfo);
             this.Controls.Add(this.cmbPaises);
             this.Name = "Form1";
             this.Text = "Form1";
             this.gbInfo.ResumeLayout(false);
             this.gbInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -115,9 +164,13 @@ namespace WeatherApp.Formularios.Formularios
 
         private System.Windows.Forms.ComboBox cmbPaises;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox gbInfo;
+        private System.Windows.Forms.Label lblHumedad;
+        private System.Windows.Forms.Label lblTemperatura;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- WeatherApp.Formularios/Formularios/Form1.cs
+++ WeatherApp.Formularios/Formularios/Form1.cs
@@ -1,10 +1,14 @@
-﻿using RESTCountries.Services;
+﻿using Newtonsoft.Json;
+using RESTCountries.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +29,78 @@ namespace WeatherApp.Formularios.Formularios
             List<string> countries = all.Select(c => c.Name).ToList();
             foreach (string pais in countries)
                 cmbPaises.Items.Add(pais);
+        }
+
+        private void  btnConfirm_Click(object sender, EventArgs e)
+        {
+            string pais = cmbPaises.SelectedItem.ToString();
+            string token = "d83e25fc5a343c4a236cda331f10b520";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={pais}&appid={token}";
+            dynamic data = GetWeather(url); ;
+            if (data == null)
+            {
+                vaciarCampos();
+                return;
+            }
+
+            double kelvin = (data.main.temp);
+            double celcius = kelvin - 273.15;
+
+            lblTiempo.Text = data.weather[0].description;
+            lblTemperatura.Text = celcius.ToString("N2") + " Cº";
+            lblHumedad.Text = data.main.humidity + "%";
+            string imagen = data.weather[0].icon;
+            string ImageUrl = $"http://openweathermap.org/img/w/{imagen}.png";
+            getWeatherPicture(ImageUrl);
+        }
+
+        private void vaciarCampos()
+        {
+            lblTiempo.Text = "EL PAIS SELECCIONADO NO SE ENCUENTRA EN LA API";
+            lblTemperatura.Text = "-------";
+            lblHumedad.Text = "-------";
+            pictureBox1.Image = null;
+        }
+
+        private dynamic GetWeather(string url)
+        {
+            try
+            {
+                WebRequest webRequest = WebRequest.Create(url);
+                HttpWebResponse httpWebResponse = null;
+                httpWebResponse = (HttpWebResponse)webRequest.GetResponse();
+
+                string result = string.Empty;
+                using (Stream stream = httpWebResponse.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(stream);
+                    result = streamReader.ReadToEnd();
+                }
+                dynamic data = JsonConvert.DeserializeObject(result);
+                return data;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void getWeatherPicture(string url)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                byte[] bytes = wc.DownloadData(url);
+
+                MemoryStream ms = new MemoryStream(bytes);
+                Image img = Image.FromStream(ms);
+
+                pictureBox1.Image = img;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
